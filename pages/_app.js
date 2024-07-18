0e1db55edb7c211776aaa645467e9fe89f106dcf
shayan-cyber <debroyshayan@gmail.com>
2024-07-18 21:18:45 +0530

    UPD:NProgress_added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import { Toaster, toast } from 'sonner'
 import { ClerkProvider } from "@clerk/nextjs";
+import NextNProgress from 'nextjs-progressbar';
 export default function App({ Component, pageProps }) {
   return(
     <>
       <ClerkProvider>
-
+        <NextNProgress color="#4b76d2" height={4} />
         <Toaster position="bottom-left" richColors/>
         <Component {...pageProps} />
       </ClerkProvider>
